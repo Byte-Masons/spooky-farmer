fab8f4fda13eee2777928772e6e4b5e1e1e1d812
tess3rac7 <tess3rac7@protonmail.com>
2022-07-13 08:08:00 -0400

    For USDC underlying.

--- contracts/ReaperStrategySpookyWftmUnderlying.sol
+++ contracts/ReaperStrategySpookyWftmUnderlying.sol
@@ -12,7 +12,7 @@ import "@openzeppelin/contracts-upgradeable/utils/math/MathUpgradeable.sol";
 /**
  * @dev Deposit SpookySwap LP tokens into MasterChef. Harvest BOO rewards and recompound.
  */
-contract ReaperStrategySpookyWftmUnderlying is ReaperBaseStrategyv3 {
+contract ReaperStrategySpookyUsdcUnderlying is ReaperBaseStrategyv3 {
     using SafeERC20Upgradeable for IERC20Upgradeable;
 
     // 3rd-party contract addresses
@@ -21,26 +21,23 @@ contract ReaperStrategySpookyWftmUnderlying is ReaperBaseStrategyv3 {
 
     /**
      * @dev Tokens Used:
-     * {WFTM} - Required for liquidity routing when doing swaps.
      * {BOO} - Reward token for depositing LP into MasterChef.
+     * {USDC} - Token to charge fees and make new LP.
      * {want} - Address of the LP token to farm. (lowercase name for FE compatibility)
      * {lpToken0} - First token of the want LP
      * {lpToken1} - Second token of the want LP
      */
-    address public constant WFTM = 0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83;
     address public constant BOO = 0x841FAD6EAe12c286d1Fd18d1d525DFfA75C7EFFE;
-    address public constant DAI = 0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E;
+    address public constant USDC = 0x04068DA6C83AFCFA0e13ba15A6696662335D5B75;
     address public want;
     address public lpToken0;
     address public lpToken1;
 
     /**
      * @dev Paths used to swap tokens:
-     * {booToDaiPath} - to swap {BOO} to {DAI} (using SPOOKY_ROUTER)
-     * {booToWftmPath} - to swap {BOO} to {WFTM} (using SPOOKY_ROUTER)
+     * {booToUsdcPath} - to swap {BOO} to {USDC} (using SPOOKY_ROUTER)
      */
-    address[] public booToDaiPath;
-    address[] public booToWftmPath;
+    address[] public booToUsdcPath;
 
     /**
      * @dev Spooky variables.
@@ -63,8 +60,7 @@ contract ReaperStrategySpookyWftmUnderlying is ReaperBaseStrategyv3 {
         __ReaperBaseStrategy_init(_vault, _feeRemitters, _strategists, _multisigRoles);
         want = _want;
         poolId = _poolId;
-        booToDaiPath = [BOO, WFTM, DAI];
-        booToWftmPath = [BOO, WFTM];
+        booToUsdcPath = [BOO, USDC];
         lpToken0 = IUniV2Pair(want).token0();
         lpToken1 = IUniV2Pair(want).token1();
     }
@@ -117,19 +113,19 @@ contract ReaperStrategySpookyWftmUnderlying is ReaperBaseStrategyv3 {
         IERC20Upgradeable boo = IERC20Upgradeable(BOO);
         uint256 booFee = (boo.balanceOf(address(this)) * totalFee) / PERCENT_DIVISOR;
         if (booFee != 0) {
-            IERC20Upgradeable dai = IERC20Upgradeable(DAI);
-            uint256 daiBalBefore = dai.balanceOf(address(this));
-            _swap(booFee, booToDaiPath);
-            uint256 daiFee = dai.balanceOf(address(this)) - daiBalBefore;
+            IERC20Upgradeable usdc = IERC20Upgradeable(USDC);
+            uint256 usdcBalBefore = usdc.balanceOf(address(this));
+            _swap(booFee, booToUsdcPath);
+            uint256 usdcFee = usdc.balanceOf(address(this)) - usdcBalBefore;
 
-            callerFee = (daiFee * callFee) / PERCENT_DIVISOR;
-            uint256 treasuryFeeToVault = (daiFee * treasuryFee) / PERCENT_DIVISOR;
+            callerFee = (usdcFee * callFee) / PERCENT_DIVISOR;
+            uint256 treasuryFeeToVault = (usdcFee * treasuryFee) / PERCENT_DIVISOR;
             uint256 feeToStrategist = (treasuryFeeToVault * strategistFee) / PERCENT_DIVISOR;
             treasuryFeeToVault -= feeToStrategist;
 
-            dai.safeTransfer(msg.sender, callerFee);
-            dai.safeTransfer(treasury, treasuryFeeToVault);
-            dai.safeTransfer(strategistRemitter, feeToStrategist);
+            usdc.safeTransfer(msg.sender, callerFee);
+            usdc.safeTransfer(treasury, treasuryFeeToVault);
+            usdc.safeTransfer(strategistRemitter, feeToStrategist);
         }
     }
 
@@ -153,21 +149,21 @@ contract ReaperStrategySpookyWftmUnderlying is ReaperBaseStrategyv3 {
 
     function _swapFromBoo() internal {
         if (lpToken0 == BOO || lpToken1 == BOO) {
-            // FTM-BOO LP
-            _swap(IERC20Upgradeable(BOO).balanceOf(address(this)) / 2, booToWftmPath);
+            // USDC-BOO LP
+            _swap(IERC20Upgradeable(BOO).balanceOf(address(this)) / 2, booToUsdcPath);
         } else {
-            // FTM-X LP, where X != BOO
-            _swap(IERC20Upgradeable(BOO).balanceOf(address(this)), booToWftmPath);
-            if (lpToken0 == WFTM) {
-                address[] memory wftmToLP1 = new address[](2);
-                wftmToLP1[0] = WFTM;
-                wftmToLP1[1] = lpToken1;
-                _swap(IERC20Upgradeable(lpToken0).balanceOf(address(this)) / 2, wftmToLP1);
+            // USDC-X LP, where X != BOO
+            _swap(IERC20Upgradeable(BOO).balanceOf(address(this)), booToUsdcPath);
+            if (lpToken0 == USDC) {
+                address[] memory usdcToLP1 = new address[](2);
+                usdcToLP1[0] = USDC;
+                usdcToLP1[1] = lpToken1;
+                _swap(IERC20Upgradeable(lpToken0).balanceOf(address(this)) / 2, usdcToLP1);
             } else {
-                address[] memory wftmToLP0 = new address[](2);
-                wftmToLP0[0] = WFTM;
-                wftmToLP0[1] = lpToken0;
-                _swap(IERC20Upgradeable(lpToken1).balanceOf(address(this)) / 2, wftmToLP0);
+                address[] memory usdcToLP0 = new address[](2);
+                usdcToLP0[0] = USDC;
+                usdcToLP0[1] = lpToken0;
+                _swap(IERC20Upgradeable(lpToken1).balanceOf(address(this)) / 2, usdcToLP0);
             }
         }
     }
