a61b3055a4e7ac1dc4c611a5dc48421a12aface2
tess3rac7 <tess3rac7@protonmail.com>
2022-07-13 19:46:34 -0400

    Deploy ftm-eth v2.

--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,6 +1,6 @@
 async function main() {
-  const vaultAddress = 'TODO';
-  const strategyAddress = 'TODO';
+  const vaultAddress = '0xA9af46DF21a45D20da73a3020f04255fDF31Eb77';
+  const strategyAddress = '0xdd35A19ced92E34D00510f85B2c21db4635dfd3f';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
   const vault = Vault.attach(vaultAddress);
