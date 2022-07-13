f9abe4e1d71dc8fda79b82778beee9a9762af933
tess3rac7 <tess3rac7@protonmail.com>
2022-07-13 19:25:16 -0400

    Deployed ftm-dai v2.

--- scripts/deploy/1-vault.js
+++ scripts/deploy/1-vault.js
@@ -1,9 +1,9 @@
 async function main() {
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
 
-  const wantAddress = '0xC9FB686f14bDA7e2653cF8F605Dc8551B6a53FD3';
-  const tokenName = 'FTM-APE Spooky Crypt';
-  const tokenSymbol = 'rf-FTM-APE';
+  const wantAddress = '0xe120ffBDA0d14f3Bb6d6053E90E63c572A66a428';
+  const tokenName = 'FTM-DAI SpookyV2 Crypt';
+  const tokenSymbol = 'rf-FTM-DAI-V2';
   const depositFee = 0;
   const tvlCap = ethers.constants.MaxUint256;
 
--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,6 +1,6 @@
 async function main() {
-  const vaultAddress = 'TODO';
-  const strategyAddress = 'TODO';
+  const vaultAddress = '0xA6c25F27b21BF7E19f2356bEA758221eC05023dF';
+  const strategyAddress = '0xf93BAE8C2A998E86f04c9Ad6A88d24847491798d';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
   const vault = Vault.attach(vaultAddress);
