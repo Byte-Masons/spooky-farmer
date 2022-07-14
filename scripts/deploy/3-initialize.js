d59ed3e9be711acd7f4e3f514fb5ef3f763b2d43
tess3rac7 <tess3rac7@protonmail.com>
2022-07-13 21:39:53 -0400

    Deploy ftm-avax v2.

--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,6 +1,6 @@
 async function main() {
-  const vaultAddress = 'TODO';
-  const strategyAddress = 'TODO';
+  const vaultAddress = '0x6CF673b45a9960E9D44aAE2b9a11CDE0B330638e';
+  const strategyAddress = '0x5e5c186F79710A68D1EFFA241AB7B95063c4fF4f';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
   const vault = Vault.attach(vaultAddress);
