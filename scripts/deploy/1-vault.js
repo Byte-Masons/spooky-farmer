b205fe8687ac4510609f80845e7312b5c9b4e4be
tess3rac7 <tess3rac7@protonmail.com>
2022-07-14 01:37:30 -0400

    Deploy ftm-alpaca v2.

--- scripts/deploy/1-vault.js
+++ scripts/deploy/1-vault.js
@@ -1,9 +1,9 @@
 async function main() {
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
 
-  const wantAddress = '0xC9FB686f14bDA7e2653cF8F605Dc8551B6a53FD3';
-  const tokenName = 'FTM-APE Spooky Crypt';
-  const tokenSymbol = 'rf-FTM-APE';
+  const wantAddress = '0xF66D2bf736c05723b62E833A5DD747E24855ff99';
+  const tokenName = 'FTM-ALPACA SpookyV2 Crypt';
+  const tokenSymbol = 'rf-FTM-ALPACA-V2';
   const depositFee = 0;
   const tvlCap = ethers.constants.MaxUint256;
 
--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,6 +1,6 @@
 async function main() {
-  const vaultAddress = 'TODO';
-  const strategyAddress = 'TODO';
+  const vaultAddress = '0x5a7c38c98E3262479bA4ac7433fbf79482BaE314';
+  const strategyAddress = '0xe7B63D7Db08fBa119470FdD36cBD196D6C62bcF8';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
   const vault = Vault.attach(vaultAddress);
