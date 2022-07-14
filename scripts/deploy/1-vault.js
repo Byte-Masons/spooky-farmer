b90fba16695314de1ceb65754aad1c4f2e50ca2a
tess3rac7 <tess3rac7@protonmail.com>
2022-07-13 20:51:37 -0400

    Deploy ftm-bnb v2.

--- scripts/deploy/1-vault.js
+++ scripts/deploy/1-vault.js
@@ -1,9 +1,9 @@
 async function main() {
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
 
-  const wantAddress = '0xC9FB686f14bDA7e2653cF8F605Dc8551B6a53FD3';
-  const tokenName = 'FTM-APE Spooky Crypt';
-  const tokenSymbol = 'rf-FTM-APE';
+  const wantAddress = '0x956DE13EA0FA5b577E4097Be837BF4aC80005820';
+  const tokenName = 'FTM-BNB SpookyV2 Crypt';
+  const tokenSymbol = 'rf-FTM-BNB-V2';
   const depositFee = 0;
   const tvlCap = ethers.constants.MaxUint256;
 
--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,6 +1,6 @@
 async function main() {
-  const vaultAddress = 'TODO';
-  const strategyAddress = 'TODO';
+  const vaultAddress = '0xAb7f592B1e4ad960461db1A34E150d1d6fD00a75';
+  const strategyAddress = '0xa7D67aC94c9b5e12193cbCBEA0850eFC8cA5b80D';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
   const vault = Vault.attach(vaultAddress);
