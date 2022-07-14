8a8c90f313af47a77fea6334dfa71abd8f2c2013
tess3rac7 <tess3rac7@protonmail.com>
2022-07-14 01:44:53 -0400

    Deploy ftm-yoshi v2.

--- scripts/deploy/1-vault.js
+++ scripts/deploy/1-vault.js
@@ -1,9 +1,9 @@
 async function main() {
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
 
-  const wantAddress = '0xC9FB686f14bDA7e2653cF8F605Dc8551B6a53FD3';
-  const tokenName = 'FTM-APE Spooky Crypt';
-  const tokenSymbol = 'rf-FTM-APE';
+  const wantAddress = '0x6bB685358BC3991D9279562710F3a44B7e5F2D9b';
+  const tokenName = 'FTM-YOSHI SpookyV2 Crypt';
+  const tokenSymbol = 'rf-FTM-YOSHI-V2';
   const depositFee = 0;
   const tvlCap = ethers.constants.MaxUint256;
 
--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,6 +1,6 @@
 async function main() {
-  const vaultAddress = 'TODO';
-  const strategyAddress = 'TODO';
+  const vaultAddress = '0x6FbA98c3cA86B6F075eC5d91cA0f38Da86701a3B';
+  const strategyAddress = '0xd76c955e071Ad5Ea4A7A8A103c0D8a409d8F7100';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
   const vault = Vault.attach(vaultAddress);
