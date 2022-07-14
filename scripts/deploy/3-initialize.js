c9730187dae37bd334c507d58ebab5c9e0a679ef
tess3rac7 <tess3rac7@protonmail.com>
2022-07-13 23:31:18 -0400

    Deploy FTM-beFTM v2.

--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,6 +1,6 @@
 async function main() {
-  const vaultAddress = 'TODO';
-  const strategyAddress = 'TODO';
+  const vaultAddress = '0x9bb44533bb9737B72Ee4153B3D40928086eEAF44';
+  const strategyAddress = '0x0674BB994D1bB256991b98d41BFB9736A0E88094';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
   const vault = Vault.attach(vaultAddress);
