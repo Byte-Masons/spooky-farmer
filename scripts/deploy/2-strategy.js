55bfe4617962983da6762a7381bee8d698aa9bb7
tess3rac7 <tess3rac7@protonmail.com>
2022-07-14 01:52:25 -0400

    Deploy ftm-brush v2.

--- scripts/deploy/2-strategy.js
+++ scripts/deploy/2-strategy.js
@@ -1,7 +1,7 @@
 const hre = require('hardhat');
 
 async function main() {
-  const vaultAddress = 'TODO';
+  const vaultAddress = '0xcfF6d40c0369EBE08F25157aF8A2e97aa5358Dc5';
 
   const Strategy = await ethers.getContractFactory('ReaperStrategySpookyWftmUnderlying');
   const treasuryAddress = '0x0e7c5313E9BB80b654734d9b7aB1FB01468deE3b';
@@ -12,8 +12,8 @@ async function main() {
   const superAdmin = '0x04C710a1E8a738CDf7cAD3a52Ba77A784C35d8CE';
   const admin = '0x539eF36C804e4D735d8cAb69e8e441c12d4B88E0';
   const guardian = '0xf20E25f2AB644C8ecBFc992a6829478a85A98F2c';
-  const wantAddress = '0xC9FB686f14bDA7e2653cF8F605Dc8551B6a53FD3';
-  const poolId = 70;
+  const wantAddress = '0x4EE115137ac73A3e5F99598564905465C101b11F';
+  const poolId = 35;
 
   const strategy = await hre.upgrades.deployProxy(
     Strategy,
--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,6 +1,6 @@
 async function main() {
-  const vaultAddress = 'TODO';
-  const strategyAddress = 'TODO';
+  const vaultAddress = '0xcfF6d40c0369EBE08F25157aF8A2e97aa5358Dc5';
+  const strategyAddress = '0xb77149ECd49078e6B7C09D9fD84bE610B6A475E8';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
   const vault = Vault.attach(vaultAddress);
