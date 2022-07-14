1941190877c6f849ec70d4f267690a0b5c5cadce
tess3rac7 <tess3rac7@protonmail.com>
2022-07-14 01:04:41 -0400

    Deploy usdc-tusd v2.

--- scripts/deploy/1-vault.js
+++ scripts/deploy/1-vault.js
@@ -1,9 +1,9 @@
 async function main() {
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
 
-  const wantAddress = '0xC9FB686f14bDA7e2653cF8F605Dc8551B6a53FD3';
-  const tokenName = 'FTM-APE Spooky Crypt';
-  const tokenSymbol = 'rf-FTM-APE';
+  const wantAddress = '0x12692B3bf8dd9Aa1d2E721d1a79efD0C244d7d96';
+  const tokenName = 'USDC-TUSD SpookyV2 Crypt';
+  const tokenSymbol = 'rf-USDC-TUSD-V2';
   const depositFee = 0;
   const tvlCap = ethers.constants.MaxUint256;
 
--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,6 +1,6 @@
 async function main() {
-  const vaultAddress = 'TODO';
-  const strategyAddress = 'TODO';
+  const vaultAddress = '0x89f728DD1CbB53a236DAF2706e119ED2da439E6c';
+  const strategyAddress = '0xA4De792516Ee47dAB76d7DDbfa3a6ae80259F490';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
   const vault = Vault.attach(vaultAddress);
