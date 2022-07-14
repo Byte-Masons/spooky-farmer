e954fd22164cb1a54767b6451027bcc71e72f930
tess3rac7 <tess3rac7@protonmail.com>
2022-07-14 02:10:51 -0400

    Deploy ftm-kek v2.

--- scripts/deploy/1-vault.js
+++ scripts/deploy/1-vault.js
@@ -1,9 +1,9 @@
 async function main() {
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
 
-  const wantAddress = '0xC9FB686f14bDA7e2653cF8F605Dc8551B6a53FD3';
-  const tokenName = 'FTM-APE Spooky Crypt';
-  const tokenSymbol = 'rf-FTM-APE';
+  const wantAddress = '0xf3582dbAEbb4287B462f00d308911621a960A5FD';
+  const tokenName = 'FTM-KEK SpookyV2 Crypt';
+  const tokenSymbol = 'rf-FTM-KEK-V2';
   const depositFee = 0;
   const tvlCap = ethers.constants.MaxUint256;
 
--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,6 +1,6 @@
 async function main() {
-  const vaultAddress = 'TODO';
-  const strategyAddress = 'TODO';
+  const vaultAddress = '0x0aaf14d7115D4649116e44F6718FA2BBaa5ef220';
+  const strategyAddress = '0x53180a6f437DA4B00c8689CCF4aFf9719541Da44';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
   const vault = Vault.attach(vaultAddress);
